55cdea68ec14fdf1dca5d13d78ea1e8adacb3fc7
random1911 <random1911@gmail.com>
2018-09-15 21:00:55 +0300

    added some style

--- src/components/styles/theme.js
+++ src/components/styles/theme.js
@@ -3,7 +3,9 @@ const theme = {
     red: opacity => `rgba(211, 47, 47, ${opacity || 1})`,
     blue: opacity => `rgba(21, 101, 192, ${opacity || 1})`,
     green: opacity => `rgba(6, 125, 50, ${opacity || 1})`,
-    borderColor: () => '#464646',
+    borderColor: () => "#464646",
+    coast: () => "#81d4fa",
+    banana: () => "#ffeb3b"
   },
   mixins: {
     visuallyHidden: () => `
@@ -20,22 +22,22 @@ const theme = {
       font-size: inherit;
       border: none;
       color: inherit;
-    `,
+    `
   },
   fonts: {
-    family: '\'Segoe Ui\', Helvetica, Roboto, sans-serif',
-    size: '16px',
+    family: "'Segoe Ui', Helvetica, Roboto, sans-serif",
+    size: "16px"
   },
   breakpoints: {
-    tabletMin: '600px',
-    tabletMedium: '768px',
-    desktopMin: '960px',
-    desktopLarger: '1340px',
+    tabletMin: "600px",
+    tabletMedium: "768px",
+    desktopMin: "960px",
+    desktopLarger: "1340px"
   },
   sizes: {
-    tile: '100px'
+    tile: "120px"
   },
-  transition: '.25s ease-in-out'
-}
+  transition: ".25s ease-in-out"
+};
 
-export default theme
+export default theme;
